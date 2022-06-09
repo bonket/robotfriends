ef34dd287a9fc9352ba8ebc57fbfaed62303efbc
Bonke <bonket@gmail.com>
2022-06-09 19:51:14 +0200

    Updates

---
+++ precache-manifest.554a6a4ff9db7fb977a9fae1aee1f014.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0575676b922fc2de7096",
+    "url": "/robotfriends/static/css/main.749e73fd.chunk.css"
+  },
+  {
+    "revision": "0575676b922fc2de7096",
+    "url": "/robotfriends/static/js/main.0575676b.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robotfriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "60801ac6a2b26e74392d",
+    "url": "/robotfriends/static/css/2.34e54651.chunk.css"
+  },
+  {
+    "revision": "60801ac6a2b26e74392d",
+    "url": "/robotfriends/static/js/2.60801ac6.chunk.js"
+  },
+  {
+    "revision": "e51a43ac8f47f9940acc",
+    "url": "/robotfriends/static/js/runtime~main.e51a43ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "3a60a1b6ab4d94118430d90d62b6b8d7",
+    "url": "/robotfriends/index.html"
+  }
+];
